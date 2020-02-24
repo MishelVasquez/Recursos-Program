d3bf4f1caf11ea1b2447923c3c2fc51948719cbb
Mishel Vasquez <gvasquez@ithwh.edu.gt>
2020-02-24 14:41:35 -0600

    No hay modificaciones

--- datos.js
+++ datos.js
@@ -2,4 +2,3 @@ var dato;
 dato = prompt("Ingrese un numero:")
 document.write("El numero que ha ingresado es: " +dato);
 console.log("El numero que ha ingresado es: " +dato);
-alert("El numero que ha ingresado es: " +dato);
