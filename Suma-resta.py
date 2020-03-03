f69eede77c1618a9449adc124d3b75d31f3f952f
MishelVasquez <61294052+MishelVasquez@users.noreply.github.com>
2020-03-03 00:23:02 -0600

    Add files via upload

---
+++ Suma-resta.py
@@ -0,0 +1,9 @@
+n1=int(input("Ingrese un numero: "))
+n2=int(input("Ingrese otro numero: "))
+
+suma=n1+n2
+resta=n1-n2
+
+print("El resultado es de la resta es:",resta)
+print("Es resultado de la suma es:",suma)
+input()
